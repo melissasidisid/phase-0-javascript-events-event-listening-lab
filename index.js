df7cf448bd2e7c6325ca62c50f4646e87a6b8651
melissa <milyssasidisaid@gmail.com>
2023-08-11 01:33:38 +0200

    done

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
-}
+function addingEventListener(){ 
+
+    const button = document.getElementById('button');
+    
+    
+    button.addEventListener("click", function addingEventListener(){
+        console.log("I was clicked")
+    })
+    }
+    addingEventListener()
